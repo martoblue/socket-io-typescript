f332335a1d9c02769a93ea4a7aef36bc9a2549e6
Martoblue <luismarto8a@gmail.com>
2024-02-02 16:53:56 -0300

    terminado el live chat con rooms

--- index.ts
+++ index.ts
@@ -12,29 +12,55 @@ const port = process.env.PORT ?? 3030;
 
 // Crear una instancia de socket.io para configurar el servidor de WebSocket
 
-// TODO: Habilitar el acceso al cliente, configurar el objeto {}
 const io = new SocketIOServer(server, {
   cors: {
     origin: '*',
     methods: ['GET', 'POST'],
   },
 });
+// Habilitar los CORS para las rutas express
 app.use(cors());
 
+// Evento de conexión: se dispara cada vez que un cliente se conecta
 io.on('connection', (socket: Socket) => {
-  console.log('Client connected');
+  console.log('a user connected');
+
+  // Uniendo automaticamente al usuario a la sala "general"
+  socket.join('general');
+
+  // Manejando el Evento join room para unirse a una sala privada
+  socket.on('join room', (room: string) => {
+    // leaveAllRoomsExceptGeneral(socket); // El usuario se salga de todas las salas excepto la general
+    socket.leave('general');
+    socket.join(room); // Uniendose a una sala privada
+    console.log(`User has joined a room: ${room}`);
+  });
+
+  // Manejando el evento 'join general' para regresar a la sale general
+  socket.on('join general', () => {
+    leaveAllRoomsExceptGeneral(socket);
+    socket.join('general');
+  });
 
   // Evento personalizado 'chat message' para recibir mensajes del cliente
-  socket.on('chat message', (msg) => {
-    // Emitir el mensaje recibido a todos los clientes
-    io.emit('chat message', msg);
+  socket.on('chat message', (data: { room: string; userName: string; message: string }) => {
+    // Emitiendo el mensaje a todos los usuarios de una sala especifica
+    io.to(data.room).emit('chat message', data);
   });
 
   // Evento de desconexión: se dispara cuando un cliente se desconecta
   socket.on('disconnect', () => {
-    console.log('Client disconnected');
+    console.log('user disconnected');
   });
 });
+
+function leaveAllRoomsExceptGeneral(socket: Socket) {
+  socket.rooms.forEach((room) => {
+    if (room !== 'general' && room !== socket.id) {
+      socket.leave(room);
+    }
+  });
+}
 
 // Iniciar el servidor para escuchar en el puerto 3000
 server.listen(port, () => {
